04f2e2eb7c95d2cb677a11a7b07231ddcfde8de2
lielmazar <liel.mazar@gmail.com>
2025-02-11 20:13:15 -0500

    task 3.2

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,32 @@
-import requests
+import requests, json
+
+# Define a maximum size limit (in bytes)
+MAX_SIZE = 10000
+
+# Calling IBM embedded NLP
+URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+# Conforming to API's rules
+def _ibm_text_requirement_format(text_to_analyse):
+    return { "raw_document": { "text": f"{text_to_analyse}" } }
+
+# There are malicious jsons! TODO
+def _safe_load_json(response):
+    content_length = len(response.content)
+    if content_length > MAX_SIZE:
+        raise ValueError("JSON data exceeds the size limit")
+    return json.loads(response.content)
 
 def emotion_detector(text_to_analyse):
-    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    JSON = { "raw_document": { "text": text_to_analyse } }
-    results = requests.post(url = URL, headers = HEADERS, json = JSON)
-    return results
-Z
+    formatted_text = _ibm_text_requirement_format(text_to_analyse)
+    response = requests.post(url = URL, headers = HEADERS, json = formatted_text)
+    try:
+        data = _safe_load_json(response) # returns python dictionary
+        # TODO 3.3
+        return data
+    except ValueError as e:
+        return f"Error: {e}"
+    except json.JSONDecodeError as e:
+        return f"Failed to decode JSON: {e}"
+    return response 
